6c70774da4d303693c5afaa9844c533a17503d91
杜哲凯 <646424878@qq.com>
2017-11-05 10:41:48 +0800

    Initial Commit

---
+++ AreaParty/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AreaParty
+//
+//  Created by 杜哲凯 on 2017/11/5.
+//  Copyright © 2017年 杜哲凯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
